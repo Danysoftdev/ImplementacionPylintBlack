88459a52a3004f2b34304e77886c73d8f667b404
alejandro-411 <castano.alejandro.3774@eam.edu.co>
2024-09-18 16:33:30 -0500

    producto model correctly documentated

---
+++ FastAPI/app/models/producto.py
@@ -0,0 +1,35 @@
+"""
+producto.py
+
+This module defines the Producto model using Pydantic's BaseModel.
+
+Classes:
+    Producto: Represents a product entity with attributes such as id, nombre,
+    descripcion, precio, and categoria_id.
+
+Attributes:
+    Decimal (module): Provides support for fast correctly-rounded decimal floating point arithmetic.
+    BaseModel (class): Pydantic's base class for creating data models.
+"""
+
+from decimal import Decimal
+from pydantic import BaseModel
+
+
+class Producto(BaseModel):
+    """
+    Producto model representing a product entity.
+
+    Attributes:
+        id (int): Unique identifier for the product.
+        nombre (str): Name of the product.
+        descripcion (str): Description of the product.
+        precio (Decimal): Price of the product.
+        categoria_id (int): Identifier for the category to which the product belongs.
+    """
+
+    id: int
+    nombre: str
+    descripcion: str
+    precio: Decimal
+    categoria_id: int
